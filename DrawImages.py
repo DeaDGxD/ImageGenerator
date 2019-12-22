039990e48d494050422762e4aedfb40b2dce92cd
MichalBronowicki <qazedcqwe1@o2.pl>
2019-12-22 11:00:35 +0100

    Read every text from file

--- DrawImages.py
+++ DrawImages.py
@@ -11,9 +11,6 @@ if not os.path.isfile(configfile_name):
     Config.create_config()
 Config.get_whole_config()
 
-if not os.path.isfile('lista_tekstow.txt'):
-    f = open('lista_tekstow.txt', 'w+')
-    f.close()
 if not os.path.isdir('images'):
     os.mkdir('images')
 if not os.path.isdir('teksty'):
@@ -27,27 +24,27 @@ def create_image():
     draw_function = DrawFunction(draw, Config, lines)
     draw_function.start_drawing()
 
-    img.save('images/'+str(text_name)+'/img'+str(x)+'.jpg')
+    img.save('images/'+folder_name+'/img'+str(x)+'.jpg')
 
 
-text_list = open('lista_tekstow.txt', mode='r', encoding='utf-8-sig')
-for text in text_list:
-    text_name = text.strip('\n')
-    os.mkdir('images/'+str(text_name))
+for file in os.listdir("teksty"):
+    if file.endswith(".txt"):
+        text_name = os.path.join("teksty", file)
+        folder_name = file[:-4]
 
-    all_lines = open('teksty/'+str(text_name)+'.txt', mode='r', encoding='utf-8-sig')
-    lines = []
-    x = 1
+        os.mkdir(os.path.join('images/', folder_name))
 
-    for line in all_lines:
-        if line.strip('\n') == "%":
-            create_image()
-            x += 1
-            lines = []
-        else:
-            lines.append(line.strip('\n'))
-    create_image()
-    all_lines.close()
-text_list.close()
+        all_lines = open(text_name, mode='r', encoding='utf-8-sig')
+        lines = []
+        x = 1
 
+        for line in all_lines:
+            if line.strip('\n') == "%":
+                create_image()
+                x += 1
+                lines = []
+            else:
+                lines.append(line.strip('\n'))
+        create_image()
+        all_lines.close()
 
